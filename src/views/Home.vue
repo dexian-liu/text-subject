dae58f9b76ac106200e7da599e59b6dec64657ee
dexian_liu <1796072065@qq.com>
2020-11-02 17:07:08 +0800

    这是测试代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    这是一个首页
+  <Deatil></Deatil>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Deatil from './Detail'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  components : {
+    Deatil
   }
 }
 </script>
